e76102fc6068cdc0bf8f3296400c149e3e316d28
Joseph Steven Martinez <32380060+josephsmartinez@users.noreply.github.com>
2018-07-24 10:59:35 -0400

    updated comments

--- AD/add.sh
+++ AD/add.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+<<'ScriptInformation'
+Author:     Chaitanya Mundhe
+Department: CASTIC-LINUX
+Last Revision: 07.24.2018
+About:      Shell Script installs dependent packages, create local user, and adds
+            system to FIUs active directory.
+Resources:  
+https://www.linuxtechi.com/integrate-rhel7-centos7-windows-active-directory/
+
+'ScriptInformation'
     
 ## Ask the user for their AD creds ##
 echo enter your ad ID 
@@ -10,28 +20,21 @@ useradd $name
 echo enter your ad password
 passwd $name
 
-
-
-
 ## Ask for department ## 
 
 echo Enter the department?
 read deptname
 
+# Install the required packages
+
 yum install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python -y 
 
 realm join ad.fiu.edu --user=$name --computer-ou="OU=Unix-Linux,OU=$deptname,OU=Computers,OU=College of Arts Sciences & Education,OU=FIU Departments,DC=ad,DC=fiu,DC=edu" -v
-
-
-
 
 ## Replace the old sssd.conf with new ##
 mv /etc/sssd/sssd.conf /etc/sssd/sssd.conf.bak
 cp ./sssd.conf /etc/sssd/sssd.conf
 chmod 600 /etc/sssd/sssd.conf
-
-
-
 
 ## Stopping and enabling services ##
 
