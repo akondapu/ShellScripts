de8296bb62c77a55a7d16de8e49ab2cd848762fb
chaitanyavarsha <chaitanyavarsha@gmail.com>
2018-07-25 10:56:04 -0400

    90 % done

--- AD/add.sh
+++ AD/add.sh
@@ -7,42 +7,63 @@
 # Resources:  
 # https://www.linuxtechi.com/integrate-rhel7-centos7-windows-active-directory/
 
-'ScriptInformation'
+#'ScriptInformation'
     
+echo "######## Add conputer to AD, press G when ready ########"
+read var
+if [ "$var" = "G" ] || [ "$var" = "g" ]
+then
 ## Ask the user for their AD creds ##
-echo "######## Enter your ActiveDirectory ID ########"
-read name
+ echo "######## Enter your ActiveDirectory ID ########"
+ read name
 
-echo "######## Creating a Local-User $name ..... ########"
+ echo "######## Creating a Local-User $name ..... ########"
 useradd $name
 
-echo "######## Enter your ActiveDirectory password ########"
-passwd $name
+ echo "######## Enter your ActiveDirectory password ########"
+ passwd $name
 
 ## Ask for department ## 
 
-echo "######## Enter the department? ########"
-read deptname
+ echo "######## Enter the department? ########"
+ read deptname
 
 # Install the required packages
 
-yum install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python -y 
+ yum install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python -y 
 
-realm join ad.fiu.edu --user=$name --computer-ou="OU=Unix-Linux,OU=$deptname,OU=Computers,OU=College of Arts Sciences & Education,OU=FIU Departments,DC=ad,DC=fiu,DC=edu" -v
+ realm join ad.fiu.edu --user=$name --computer-ou="OU=Unix-Linux,OU=$deptname,OU=Computers,OU=College of Arts Sciences & Education,OU=FIU Departments,DC=ad,DC=fiu,DC=edu" -v
 
 ## Replace the old sssd.conf with new ##
-mv /etc/sssd/sssd.conf /etc/sssd/sssd.conf.bak
-cp ./sssd.conf /etc/sssd/sssd.conf
-chmod 600 /etc/sssd/sssd.conf
+ mv /etc/sssd/sssd.conf /etc/sssd/sssd.conf.bak
+ cp ./sssd.conf /etc/sssd/sssd.conf
+ chmod 600 /etc/sssd/sssd.conf
 
 ## Stopping and enabling services ##
 
-echo "######## Mending the service ....... ########"
+ echo "######## Mending the service ....... ########"
 
-service sssd stop
+ service sssd stop
 
-authconfig --enablesssd --update
+ authconfig --enablesssd --update
 
-authconfig --enablesssd --update --enablesssdauth
+ authconfig --enablesssd --update --enablesssdauth
 
-service sssd start
+ service sssd start
+fi
+
+############################## Disable Login screen ############################
+
+
+echo "######## Disable login list, press G when ready ########"
+read var3
+if [ "$var3" = "G" ] || [ "$var3" = "g" ]
+then
+  echo "######## Creating file ########"
+  touch /etc/dconf/db/gdm.d/00-login-screen
+  echo "[org/gnome/login-screen]" > /etc/dconf/db/gdm.d/00-login-screen
+  echo "#Do not show the user list" >> /etc/dconf/db/gdm.d/00-login-screen
+  echo "disable-user-list=true" >> /etc/dconf/db/gdm.d/00-login-screen
+  echo "######## updating Dconf ########"
+  dconf update
+fi
--- General/general.sh
+++ General/general.sh
@@ -11,3 +11,22 @@ then
   read enthost
   hostnamectl set-hostname "($enthost).ad.fiu.edu"
 fi
+
+
+
+echo "######## Do you wish to update ? (Y/N) ########"
+read var2
+if [ "$var2" = "Y" ] || [ "$var2" = "y" ]
+then
+  yum update -y
+fi
+
+echo "######## Press G, when ready for Time sync ########"
+read var3
+if [ "$var3" = "G" ] || [ "$var3" = "g" ]
+then
+  hwclock --systohc
+  echo "server time.fiu.edu prefer" >> /etc/chrony.conf
+  systemctl restart chronyd
+  echo "Done !!"
+fi
