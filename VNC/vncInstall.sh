8309af55c0c7833b965d9ff9222708947813ac72
Joseph <josephsmartinez@gmail.com>
2018-07-25 14:48:43 -0400

    Added directories and Ideas

---
+++ VNC/vncInstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Author:               Joseph Martinez
+# Department:           CASTIC-LINUX
+# Last Revision:        07.24.2018
+# About:                VNC installation 
+# Resources:  
