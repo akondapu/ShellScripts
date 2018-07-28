3a5d6b0d529a4ec7316367b834e1860efff47cd6
josephsmartinez <josephsmartinez@gmail.com>
2018-07-28 19:04:14 -0400

    updated Sublime

--- Software/sublime.sh
+++ Software/sublime.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # Installation for Sublime text editor on CentOS/RHEL 
 
-sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-sudo yum-config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-sudo yum install sublime-text
+
+installed=`rpm -qa | grep sublime | wc -l`
+if [ "$installed" = 0 ]; then
+
+  sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+  sudo yum-config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+  sudo yum install sublime-text
+
+else
+ 
+  echo "Software already installed!"
+  echo `rpm -qa | grep sublime` 
+
+fi
